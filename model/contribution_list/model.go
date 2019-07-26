3472e6c0446a6f6da6c54c96fb9eae8ea03a7fdf
peilong <peilong@zhuang.com>
2019-07-26 11:58:26 +0800

    add es

--- model/contribution_list/model.go
+++ model/contribution_list/model.go
@@ -1,16 +1,36 @@
 package contribution_list
 
+import (
+	"bbs_feed/lib/helper"
+	"bbs_feed/search"
+	"github.com/astaxie/beego/logs"
+)
+
 const (
-	Publish_Thread         = "publish_thread"   //发帖
-	Thread_Replied         = "thread_replied"   //帖子被回复
-	Thread_Collected       = "thread_collected" //帖子被收藏
-	Thread_Supported       = "thread_supported" //帖子被加分
 	Publish_Thread_Score   = 3                  //发帖得分权重
 	Thread_Replied_Score   = 1                  //帖子被回复得分权重
 	Thread_Collected_Score = 30                 //帖子被收藏得分权重
 	Thread_Supported_Score = 10                 //帖子被加分得分权重
 )
 
-func Get() {
 
+
+func GetActionScore(useraAction search.UserAction) int {
+	return useraAction.PublishThread * Publish_Thread_Score + useraAction.ThreadReplied * Thread_Replied_Score + useraAction.ThreadCollected * Thread_Collected_Score + useraAction.ThreadSupported * Thread_Supported_Score
+}
+
+func GetContributeScore() (map[string][]*search.User, error) {
+	index := helper.GetWeekStart().Format("2006-01-02")
+	esDatas, err := search.Search(index)
+	if err != nil {
+		logs.Error(err)
+		return nil, err
+	}
+
+	for _, v := range esDatas{
+		for _, data := range v{
+			data.Score = GetActionScore(data.Action)
+		}
+	}
+	return esDatas, nil
 }
--- model/popularity_list/model.go
+++ model/popularity_list/model.go
@@ -3,7 +3,7 @@ package popularity_list
 import (
 	"bbs_feed/lib/helper"
 	"bbs_feed/search"
-	"fmt"
+	"github.com/astaxie/beego/logs"
 )
 
 const (
@@ -11,21 +11,18 @@ const (
 	Publish_Thread_Score   = 3                  //发帖权重
 	Publish_Post_Score     = 1                  //评论权重
 	Post_Supported_Score   = 5                  //评论被加分权重
-	Thread_Supported       = "thread_supported" //帖子被加分
-	Publish_Thread         = "publish_thread"   //发帖
-	Publish_Post           = "publish_post"     //评论
-	Post_Supported         = "post_supported"   //评论被加分
 )
 
 func GetActionScore(useraAction search.UserAction) int {
-	return useraAction.ThreadSupported * 10 + useraAction.PublishThread * 3 + useraAction.PublishPost + useraAction.PostSupported * 5
+	return useraAction.ThreadSupported * Thread_Supported_Score + useraAction.PublishThread * Publish_Thread_Score + useraAction.PublishPost * Publish_Post_Score + useraAction.PostSupported * Post_Supported_Score
 }
 
-func GetUserAction() {
+func GetPopularityScore() (map[string][]*search.User, error) {
 	index := helper.GetWeekStart().Format("2006-01-02")
 	esDatas, err := search.Search(index)
 	if err != nil {
-		fmt.Println(err)
+		logs.Error(err)
+		return nil, err
 	}
 
 	for _, v := range esDatas{
@@ -33,5 +30,5 @@ func GetUserAction() {
 			data.Score = GetActionScore(data.Action)
 		}
 	}
-	fmt.Println(esDatas)
+	return esDatas, nil
 }
