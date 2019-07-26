99eeaecb553f639cdd976eea1440d4647e5be179
nian.zhou <nian.zhou@office.feng.com>
2019-07-26 11:21:23 +0800

    添加贡献榜agent

--- search/search.go
+++ search/search.go
@@ -25,19 +25,17 @@ func addIndexPrefix(index string) string {
 
 type UserAction struct {
 	ThreadSupported int `json:"thread_supported"` //帖子被加分
-	ThreadReplied   int `json:"thread_replied"` //帖子被回复
+	ThreadReplied   int `json:"thread_replied"`   //帖子被回复
 	ThreadCollected int `json:"thread_collected"` //帖子被收藏
 	PublishThread   int `json:"publish_thread"`   //发帖
-	PublishPost     int `json:"publish_post"`    //评论
-	PostSupported   int `json:"post_supported"`  //评论被加分
+	PublishPost     int `json:"publish_post"`     //评论
+	PostSupported   int `json:"post_supported"`   //评论被加分
 }
 
 type User struct {
-	Uid string
+	Uid    string
 	Action UserAction
 }
-
-
 
 func Search(index string) (map[string][]User, error) {
 	index = addIndexPrefix(index)
@@ -67,14 +65,14 @@ func Search(index string) (map[string][]User, error) {
 				return nil, err
 			}
 			user := User{
-				Uid: uid,
+				Uid:    uid,
 				Action: *item,
 			}
 			if _, ok := dataMap[fid]; ok {
 				dataMap[fid] = append(dataMap[fid], user)
 				continue
 			}
-			data := make([]User,0)
+			data := make([]User, 0)
 			data = append(data, user)
 			dataMap[fid] = data
 		}
--- service/kernel/call_block/week_popularity.go
+++ service/kernel/call_block/week_popularity.go
@@ -1,13 +1,21 @@
 package call_block
 
 import (
+	"bbs_feed/boot"
 	"bbs_feed/service"
+	"bbs_feed/service/data_source"
+	"bbs_feed/service/kernel/contract"
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/astaxie/beego/logs"
 	"time"
 )
 
 //周人气榜
+
+const CALl_BLOCK_WEEK_POPULARITY = "call_block_week_popularity"
+const CALl_BLOCK_WEEK_POPULARITY_TRAIT = "call_block_week_popularity_trait"
 
 type WeekPopularityRule struct {
 	CronExp time.Duration `json:"cronExp"` // 周期时间
@@ -15,15 +23,17 @@ type WeekPopularityRule struct {
 }
 
 type WeekPopularity struct {
-	name string
+	topicId int
+	name    string
 
-	reportChan         chan []string
 	weekPopularityRule WeekPopularityRule
 
 	cancel context.CancelFunc
 	Ctx    context.Context
 
 	topicIds []string // 数据源
+
+	contract.UserRep //违规用户
 }
 
 func NewWeekPopularity(topicId int, topicIds []string) *WeekPopularity {
@@ -33,19 +43,14 @@ func NewWeekPopularity(topicId int, topicIds []string) *WeekPopularity {
 	}
 }
 
-func (this *WeekPopularity) RemoveReportUser() {
-	for {
-		select {
-		case uids := <-this.reportChan:
-			// todo clear redis uids
-			fmt.Println(uids)
-		}
-	}
+// 删除 redis 数据
+func (this *WeekPopularity) Remover(uids []int) {
+	this.remover(uids)
 }
 
-func (this *WeekPopularity) AcceptSign(userIds []string) {
-	this.reportChan <- userIds
-	return
+func (this *WeekPopularity) remover(uids []int) {
+	logs.Info("remove --", this.redisKey(), "--", this.traitRedisKey(), "--", uids)
+	data_source.DelRedisThreadInfo(uids, this.redisKey(), this.traitRedisKey())
 }
 
 func (this *WeekPopularity) GetThis() interface{} {
@@ -57,33 +62,46 @@ func (this *WeekPopularity) Init() {
 	this.Ctx = ctx
 	this.cancel = cancel
 	this.weekPopularityRule = weekPopularity
+	go this.RemoveReportUser(this.remover) // 开启违规用户自检
 }
 
-func (this *WeekPopularity) ChangeConf(conf interface{}) {
-	if conf, ok := conf.(WeekPopularityRule); ok {
-		this.weekPopularityRule = conf
-		this.reStart()
+func (this *WeekPopularity) ChangeConf(conf string) error {
+	var rule WeekPopularityRule
+	if err := json.Unmarshal([]byte(conf), &rule); err == nil {
+		this.weekPopularityRule = rule
+		go this.reStart()
+		return nil
+	} else {
+		return err
 	}
 }
 
 func (this *WeekPopularity) Start() {
-	t := time.NewTimer(this.weekPopularityRule.CronExp)
+	this.worker()
+	t := time.NewTimer(time.Duration(this.weekPopularityRule.CronExp) * time.Minute)
 	for {
 		select {
 		case <-t.C:
-			// todo  根据配置 写redis数据
-			t.Reset(this.weekPopularityRule.CronExp)
+			this.worker()
+			t.Reset(time.Duration(this.weekPopularityRule.CronExp) * time.Minute)
 		case <-this.Ctx.Done():
 			return
 		}
 	}
 }
 
+func (this *WeekPopularity) worker() {
+	//todo
+}
+
 func (this *WeekPopularity) ChangeFids(topicIds []string) {
 	this.topicIds = topicIds
+	go this.reStart()
 }
 
 func (this *WeekPopularity) Stop() {
+	boot.InstanceRedisCli(boot.CACHE).Del(this.redisKey())
+	logs.Info(this.redisKey(), "delete success")
 	this.cancel()
 }
 
@@ -95,4 +113,17 @@ func (this *WeekPopularity) reStart() {
 	this.Stop()
 	this.Ctx, this.cancel = context.WithCancel(context.Background())
 	this.Start()
+}
+
+func (this *WeekPopularity) AddTrait(id string, trait service.CallBlockTrait) {
+	if traitBytes, err := json.Marshal(&trait); err == nil {
+		boot.InstanceRedisCli(boot.CACHE).HSet(this.traitRedisKey(), id, string(traitBytes))
+	}
+}
+func (this *WeekPopularity) redisKey() string {
+	return fmt.Sprintf("%s%s%d", CALl_BLOCK_WEEK_POPULARITY, service.Separator, this.topicId)
+}
+
+func (this *WeekPopularity) traitRedisKey() string {
+	return fmt.Sprintf("%s%s%d", CALl_BLOCK_WEEK_POPULARITY_TRAIT, service.Separator, this.topicId)
 }
